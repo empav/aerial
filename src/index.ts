3f40020d673d9989749c3cb918233c7b3a361417
Emanuele Pavan <emanuele.pav@gmail.com>
2023-07-20 11:17:31 +0200

    Exports types && aerial css
    * extracts aerial.min.css for distribution
    * exports types && bumps version

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
 import './tailwind.css';
 
 import Button from './components/Button';
+import type { ButtonProps } from './components/Button';
 import TextField from './components/TextField';
+import type { TextFieldProps } from './components/TextField';
 
+/* Components */
 export { Button, TextField };
+
+/* Types */
+export type { ButtonProps, TextFieldProps };
